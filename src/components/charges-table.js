24cce709d4c9f8ae78d98c37080a3d223f0f476a
Andy <andyjung5591@gmail.com>
2024-12-12 14:45:40 -0500

    create_expense

--- src/components/charges-table.js
+++ src/components/charges-table.js
@@ -1,18 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import './styling/charges-table.css'; 
+import React, { useState, useEffect } from "react";
+import "./styling/charges-table.css";
+import { useAuth } from "../AuthContext";
 
 const ChargesTable = ({ action }) => {
   const [charges, setCharges] = useState([]);
-
+  const apiURL = process.env.REACT_APP_EXPENSE_SERVICE_API_BASE_URL;
+  const { user } = useAuth();
   useEffect(() => {
     const fetchCharges = async () => {
-      //  REPLACE WITH API CALL
-      const simulatedData = [
-        { date_posted: '09/22/2024', group: 'cache-me-outside', name: 'John Doe', amount: '$43', reason: 'Dinner' },
-        { date_posted: '09/19/2024', group: 'cache-me-outside', name: 'John Doe', amount: '$7', reason: 'Lunch' },
-        { date_posted: '09/19/2024', group: 'cache-me-outside', name: 'John Doe', amount: '$13', reason: 'Lunch' },
-      ];
-      setCharges(simulatedData);
+      try {
+        const response = await fetch(
+          `${apiURL}/expenses/${action === "Remind" ? "payee" : "payer"}/${
+            user.uid
+          }`,
+          {
+            method: "GET",
+          }
+        );
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.detail || "Failed to fetch expenses");
+        }
+
+        const data = await response.json();
+        setCharges(data);
+      } catch (err) {}
     };
 
     fetchCharges();
@@ -24,10 +37,10 @@ const ChargesTable = ({ action }) => {
         <tr>
           <th className="th">Date Posted</th>
           <th className="th">Name</th>
-          <th className='th'>Group</th>
+          <th className="th">Group</th>
           <th className="th">Amount</th>
           <th className="th">Reason</th>
-          <th className='th'></th>
+          <th className="th">Action</th>
         </tr>
       </thead>
       <tbody>
@@ -38,7 +51,7 @@ const ChargesTable = ({ action }) => {
             <td className="td">{row.group}</td>
             <td className="td">{row.amount}</td>
             <td className="td">{row.reason}</td>
-            <td className='td'>{action}</td>
+            <td className="td">{action}</td>
           </tr>
         ))}
       </tbody>
