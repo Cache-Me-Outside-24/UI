a2ec894f824e24dc251257dc644b831e7ad644ea
Lexis Sablan <lexissablan@gmail.com>
2024-12-15 22:29:51 -0500

    demo video

--- src/components/default_home.js
+++ src/components/default_home.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./styling/default_home.css";
 import Button from "./button";
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 function HomeDefault() {
   return (
     <div className="home-content-default">
       <div className="left-home-content">
         <h1>Cut bills, not ties</h1>
-        <h2>Track shared expenses, split costs fairly, and settle up instantly—all in one place.</h2>
+        <h2>
+          Track shared expenses, split costs fairly, and settle up instantly—all
+          in one place.
+        </h2>
       </div>
       <div className="right-home-content">
         <div className="demo-box">
-            DEMO GOES HERE
+          <video
+            className="demo-video"
+            src="/assets/demo_vid.mov"
+            controls
+            autoPlay
+            loop
+            muted
+          >
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </div>
